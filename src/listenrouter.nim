99d865a409941f161d8a3f25c7c02bf762dfb2de
Moonsea Skyfish <moon.sea.sky.fish@gmail.com>
2022-03-09 20:57:55 +0900

    first commit

---
+++ src/listenrouter.nim
@@ -0,0 +1,23 @@
+import jester
+import macros, os
+
+export jester
+
+macro listenRouter*(prmPort: int, stmt: untyped): untyped =
+  template makeRouter(prmPort: int, stmt: untyped): untyped =
+    router templateRouter:
+      stmt
+    let settings = newSettings(
+      port = Port(prmPort),
+      staticDir = getCurrentDir() & "/publicx"
+    )
+    var j = initJester(templateRouter, settings = settings)
+    j.serve()
+  getAST(makeRouter(prmPort, stmt[0]))
+
+
+when isMainModule:
+  listenRouter(5555):
+    get "/":
+      resp "<p>hello, world.</p>"
+
